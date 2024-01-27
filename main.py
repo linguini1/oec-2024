c324b39440262984d5b8c75346f4ec4e78b4da1c
Matteo Golin <matteo.golin@gmail.com>
2024-01-27 09:06:12 -0500

    Added pages for reading and spelling.

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/reading")
+def reading():
+    return render_template("reading.html")
+
+
+@app.route("/spelling")
+def spelling():
+    return render_template("spelling.html")
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=PORT, debug=True)
